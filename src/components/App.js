2ba495ced7cef1f4441ea319828a8a154fb60aba
Tristan Emmerson <tristan@stickerslug.com>
2020-09-18 10:26:05 -0700

    update App.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import DrinkControl from "./DrinkControl";
+
+import "../App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <DrinkControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
