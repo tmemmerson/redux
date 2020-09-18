1da56434e668d4939a402f0b566f2ac413cc7b98
Tristan Emmerson <tristan@stickerslug.com>
2020-09-18 15:46:06 -0700

    drop drinklist into a container

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,30 +1,32 @@
 import React from "react";
 import Drink from "./Drink";
 import PropTypes from "prop-types";
-import jumbotron from "react-bootstrap/Button";
+import jumbotron from "react-bootstrap/Jumbotron";
 
 function DrinkList(props) {
   return (
     <React.Fragment>
-      <div class="jumbotron opacity90">
-        <hr />
-        {Object.values(props.drinkList)
-          .map((drink) => (
-            <Drink
-              whenDrinkClicked={props.onDrinkSelection}
-              whenUptapClicked={props.onUptapSelection}
-              whenDowntapClicked={props.onDowntapSelection}
-              name={drink.name}
-              brand={drink.brand}
-              flavor={drink.flavor}
-              price={drink.price}
-              picture={drink.picture}
-              id={drink.id}
-              tap={drink.tap}
-              key={drink.id}
-            />
-          ))
-          .sort((a, b) => b.props.tap - a.props.tap)}
+      <div class="container">
+        <div class="jumbotron opacity90">
+          <hr />
+          {Object.values(props.drinkList)
+            .map((drink) => (
+              <Drink
+                whenDrinkClicked={props.onDrinkSelection}
+                whenUptapClicked={props.onUptapSelection}
+                whenDowntapClicked={props.onDowntapSelection}
+                name={drink.name}
+                brand={drink.brand}
+                flavor={drink.flavor}
+                price={drink.price}
+                picture={drink.picture}
+                id={drink.id}
+                tap={drink.tap}
+                key={drink.id}
+              />
+            ))
+            .sort((a, b) => b.props.tap - a.props.tap)}
+        </div>
       </div>
     </React.Fragment>
   );
